565f1b730fe9fe68c39093cfc5005b03efd1ee54
Christoph Mussenbrock <christoph@etherisc.com>
2021-04-09 19:58:35 +0200

    Reformating, and deactivating oracles for testing

--- test/FlightDelayStaking.js
+++ test/FlightDelayStaking.js
@@ -1,21 +1,25 @@
 // const { expect } = require("chai");
-const FlightDelayStaking = artifacts.require("FlightDelayStaking");
-const MockDip = artifacts.require("MockDip");
+const FlightDelayStaking = artifacts.require('FlightDelayStaking');
+const MockDip = artifacts.require('MockDip');
 
-contract("FlightDelayStaking", async accounts => {
+contract('FlightDelayStaking', async (accounts) => {
   const [owner] = accounts;
 
   let flightDelayStaking;
   let mockDip;
 
-  beforeEach(async function() {
+  const halfEth = web3.utils.toWei('0.5', 'ether');
+  const oneEth = web3.utils.toWei('1', 'ether');
+  const fiveEth = web3.utils.toWei('5', 'ether');
+
+  beforeEach(async () => {
     mockDip = await MockDip.new(
-      "Mock Dip",
-      "MDIP",
-      web3.utils.toWei("100000", "ether")
+      'Mock Dip',
+      'MDIP',
+      web3.utils.toWei('100000', 'ether'),
     );
 
-    console.log("MockDip is deployed at", mockDip.address);
+    console.log('MockDip is deployed at', mockDip.address);
 
     flightDelayStaking = await FlightDelayStaking.new(mockDip.address);
 
@@ -23,93 +27,54 @@ contract("FlightDelayStaking", async accounts => {
     await flightDelayStaking.setExposureFactor(10);
 
     console.log(
-      "FlightDelayStaking is deployed at",
-      flightDelayStaking.address
+      'FlightDelayStaking is deployed at',
+      flightDelayStaking.address,
     );
   });
 
-  it("should able to Stake", async () => {
-    const requiredDips = await flightDelayStaking.calculateRequiredDip(
-      web3.utils.toWei("1", "ether")
-    );
-
+  it('should able to Stake', async () => {
+    const requiredDips = await flightDelayStaking.calculateRequiredDip(oneEth);
     const ownerDipAmount = new web3.utils.BN(await mockDip.balanceOf(owner));
 
-    await mockDip.approve(flightDelayStaking.address, ownerDipAmount, {
-      from: owner
-    });
+    await mockDip.approve(flightDelayStaking.address, ownerDipAmount, { from: owner });
+    await flightDelayStaking.stake(requiredDips, { value: oneEth });
 
-    await flightDelayStaking.stake(requiredDips, {
-      value: web3.utils.toWei("1", "ether")
-    });
-
-    expect(await web3.eth.getBalance(flightDelayStaking.address)).to.equal(
-      web3.utils.toWei("1", "ether"),
-      "Contract eth balance is not correct after staking"
-    );
-
-    console.log(ownerDipAmount.toString());
-
-    expect((await mockDip.balanceOf(owner)).toString()).to.equal(
-      ownerDipAmount.sub(requiredDips).toString(),
-      "Owner dip balance is not correct after staking"
-    );
-
-    expect(
-      (await mockDip.balanceOf(flightDelayStaking.address)).toString()
-    ).to.equal(
-      requiredDips.toString(),
-      "Contract dip balance is not correct after staking"
-    );
-
-    expect((await flightDelayStaking.getStake(owner))[1].toString()).to.equal(
-      requiredDips.toString(),
-      "Total stake of owner is not correct"
-    );
+    expect(await web3.eth.getBalance(flightDelayStaking.address))
+      .to.equal(oneEth, 'Contract eth balance is not correct after staking');
+    expect((await mockDip.balanceOf(owner)).toString())
+      .to.equal(ownerDipAmount.sub(requiredDips).toString(), 'Owner dip balance is not correct after staking');
+    expect((await mockDip.balanceOf(flightDelayStaking.address)).toString())
+      .to.equal(requiredDips.toString(), 'Contract dip balance is not correct after staking');
+    expect((await flightDelayStaking.getStake(owner))[1].toString())
+      .to.equal(requiredDips.toString(), 'Total stake of owner is not correct');
   });
 
-  it("should able to unstake", async () => {
-    const requiredDips = await flightDelayStaking.calculateRequiredDip(
-      web3.utils.toWei("1", "ether")
-    );
-
+  it('should able to unstake', async () => {
+    const requiredDips = await flightDelayStaking.calculateRequiredDip(oneEth);
     const ownerDipAmount = new web3.utils.BN(await mockDip.balanceOf(owner));
 
-    await mockDip.approve(flightDelayStaking.address, ownerDipAmount, {
-      from: owner
-    });
+    await mockDip.approve(flightDelayStaking.address, ownerDipAmount, { from: owner });
+    await flightDelayStaking.stake(requiredDips, { value: oneEth });
 
-    await flightDelayStaking.stake(requiredDips, {
-      value: web3.utils.toWei("1", "ether")
-    });
+    expect((await flightDelayStaking.getUnlockedStakeFor(owner)).toString())
+      .to.equal(oneEth);
+    expect((await flightDelayStaking.calculateRequiredDip(halfEth)).toString())
+      .to.equal(fiveEth);
 
-    expect(
-      (await flightDelayStaking.getUnlockedStakeFor(owner)).toString()
-    ).to.equal(web3.utils.toWei("1", "ether"));
-    expect(
-      (
-        await flightDelayStaking.calculateRequiredDip(
-          web3.utils.toWei("0.5", "ether")
-        )
-      ).toString()
-    ).to.equal(web3.utils.toWei("5", "ether"));
+    await flightDelayStaking.unstake(halfEth);
 
-    await flightDelayStaking.unstake(web3.utils.toWei("0.5", "ether"));
-
-    expect(await web3.eth.getBalance(flightDelayStaking.address)).to.equal(
-      web3.utils.toWei("0.5", "ether")
-    );
-    expect(
-      (await flightDelayStaking.getUnlockedStakeFor(owner)).toString()
-    ).to.equal(web3.utils.toWei("0.5", "ether"));
-    expect(
-      (await flightDelayStaking.getUnlockedStakeFor(owner)).toString()
-    ).to.equal(web3.utils.toWei("0.5", "ether"));
+    expect(await web3.eth.getBalance(flightDelayStaking.address))
+      .to.equal(halfEth);
+    expect((await flightDelayStaking.getUnlockedStakeFor(owner)).toString())
+      .to.equal(halfEth);
 
     const stakeRes = await flightDelayStaking.getStake(owner);
-    expect(stakeRes[0].toString()).to.equal(web3.utils.toWei("0.5", "ether"));
-    expect(stakeRes[1].toString()).to.equal(web3.utils.toWei("5", "ether"));
+    expect(stakeRes[0].toString())
+      .to.equal(halfEth);
+    expect(stakeRes[1].toString())
+      .to.equal(fiveEth);
   });
 
-  it("should able to purchase premium", async () => {});
+  it('should able to purchase premium', async () => {
+  });
 });
